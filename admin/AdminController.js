89fcc9cbcb3a85afcb7aacb96d927b3d2182093d
Gustavo Tenório <gltenorio@hotmail.com>
2021-05-02 12:11:41 -0300

    teste

--- admin/AdminController.js
+++ admin/AdminController.js
@@ -2,8 +2,8 @@ const express = require('express');
 
 const router = express.Router();
 
-router.get('/admin/categorias', (req,res)=>{
+router.get('/admin/categorias/new',(req,res)=>{
     res.render('./admin/categorias');
-});
+})
 
 module.exports = router;
