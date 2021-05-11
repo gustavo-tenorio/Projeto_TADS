d76d38cdaff00e16aef1a34a7f39d621246d1cb7
Gustavo Tenório <gltenorio@hotmail.com>
2021-05-11 15:15:57 -0300

    Salvando categorias no banco

--- categorias/CategoriasController.js
+++ categorias/CategoriasController.js
@@ -1,10 +1,18 @@
 const express = require('express');
 const router = express.Router();
 
-const Categorias = require('./Categorias');
+const Categorias = require('./categorias');
 
 router.get('/admin/categorias/new',(req,res)=>{
     res.render('../views/admin/categorias');
 });
 
+
+router.post('/categorias/save',(req,res)=>{
+    let categorias = req.body.categoria;
+
+    Categorias.create({
+        categoria: categorias,
+    }).then(res.json({categorias}));
+})
 module.exports = router;
