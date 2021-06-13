d73acc843e6ce85579464b43a5385e7635dae8d6
Biwceni <fabio.ormeni@hotmail.com>
2021-06-13 14:45:11 -0300

    Projeto Completo

--- database/database.js
+++ database/database.js
@@ -1,7 +1,7 @@
 const Sequelize = require('sequelize');
 
 
-const connection = new Sequelize('projetonode','root','110804',{
+const connection = new Sequelize('projetnode','root','20023005',{
     host:'localhost',
     dialect:'mysql',
     timezone:'-03:00'
